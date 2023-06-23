dacd07609e4e0d2157bd6f58aa9dc6a0128a53c5
emifa <emifa@LAPTOP-1TM449QB>
2023-06-23 13:39:22 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-5-.NET/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("enter the file path ");
+        string percorsoFile = Console.ReadLine();
+        string contenutoFile = File.ReadAllText(percorsoFile);
+        Console.WriteLine("File: ");
+        Console.WriteLine(contenutoFile);
+    }
+}
